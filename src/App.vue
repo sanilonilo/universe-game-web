75cf46e5131f1db4cdb68e7a14aef27bbea5b590
JoãoPaulo-dev-lab <paulo.j.jp89@gmail.com>
2024-06-07 21:07:17 -0300

    Inicializacao do header

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,25 @@
 </script>
 
 <template>
+    <header class="flex px-20 py-4 bg-slate-50">
+        <div class="flex items-center gap-x-5">
+            <img src="./assets/logo-microsoft.png" alt="" class="max-w-[103px]">
+            <div class="w-[2px] h-[25px] bg-black"></div>
+            <img src="./assets/logo-xbox.png" alt="" class="max-w-[70px]">
+            <nav class="hidden md:flex space-x-6">
+                <a href="#" class="hover:underline text-[13px]">Game Pass</a>
+                <a href="#" class="hover:underline text-[13px]">Jogos</a>
+                <a href="#" class="hover:underline text-[13px]">Dispositivos</a>
+                <a href="#" class="hover:underline text-[13px]">Jogar</a>
+                <a href="#" class="hover:underline text-[13px]">Loja</a>
+                <a href="#" class="hover:underline text-[13px]">Comunidade</a>
+                <a href="#" class="hover:underline text-[13px]">Suporte</a>
+                <a href="#" class="hover:underline text-[13px]">Mais</a>
+            </nav>
+        </div>
+        <div class="flex px-28 items-center gap-x-5">
+            <a href="#" class="hover:underline text-[13px]">Toda a Microsoft</a>
+        </div>
+    </header>
 </template>
 
