720e0eefd8ec03de49a610f5dd57b655c3170863
JoãoPaulo-dev-lab <paulo.j.jp89@gmail.com>
2024-06-10 20:51:47 -0300

    a

---
+++ src/components/dropdown/NavItemDropdown.vue
@@ -0,0 +1,35 @@
+<template>
+    <n-dropdown :show="showDropdown" :options="options" @select="handleSelect">
+        <span class="hover:underline text-[13px] cursor-pointer" @click="handleClick">
+            News old boys
+        </span>
+    </n-dropdown>
+</template>
+
+<script lang="ts">
+import { useMessage } from 'naive-ui';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'NavItemDropdown',
+    setup(){
+        const message = useMessage()
+        const showDropdownRef = ref(false)
+        return {
+            options: [
+                {
+                    label: "da Argentina",
+                    value: "da Argentina"
+                }
+            ],
+            showDropdown: showDropdownRef,
+            handleSelect (key: string | number) {
+            message.info(String(key))
+            },
+            handleClick () {
+            showDropdownRef.value = !showDropdownRef.value
+            }
+        }
+    }
+})
+</script>
---
+++ src/components/templates/header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="flex px-20 py-4 bg-slate-50">
+        <div class="flex items-center gap-x-5">
+            <img src="../../../assets/logo-microsoft.png" alt="" class="max-w-[106px]">
+            <div class="w-[2px] h-[25px] bg-black"></div>
+            <img src="../../../assets/logo-xbox.png" alt="" class="max-w-[70px]">
+            <nav class="hidden md:flex space-x-6">
+                <NavItemDropdown/>
+            </nav>
+        </div>
+        <div class="flex px-28 items-center gap-x-5">
+            <a href="#" class="hover:underline text-[13px]">Toda a Microsoft</a>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavItemDropdown from '../../dropdown/NavItemDropdown.vue'
+
+export default defineComponent({
+    name: 'Header',
+    components: {
+        NavItemDropdown
+    }
+})
+</script>
