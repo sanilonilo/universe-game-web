2933a283edeb86173dde6dd8454ef18661e32ec2
Sanilo Alves <saniloalves0@gmail.com>
2024-07-17 22:17:19 -0300

    refact: add display grid in icon component

--- src/components/icons/Icons.vue
+++ src/components/icons/Icons.vue
@@ -1,38 +1,36 @@
 <template>
-    <div class="grid grid-cols-6 justify-items-center p-[3rem] w-[90%] gap-[3rem] max-w-[90rem]">
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/gamePass.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">Game Pass</p>
+    <div class="w-full grid gap-x-[50px] grid-cols-3 min-[1060px]:grid-cols-8 justify-center px-[30px] min-[1060px]:px-[60px] py-[30px]">
+        <div class="col-span-3 min-[1060px]:col-span-1"></div>
+        <a href="#" class="col-span-1 flex items-center flex-col mb-10 min-[1060px]:mb-0 hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/gamePass.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">GAME PASS</p>
         </a>
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/control.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">JOGOS</p>
+        <a href="#" class="col-span-1 flex items-center flex-col mb-10 min-[1060px]:mb-0 hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/control.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">JOGOS</p>
         </a>
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/consoles.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">CONSOLES</p>
+        <a href="#" class="col-span-1 flex items-center flex-col mb-10 min-[1060px]:mb-0 hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/consoles.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">CONSOLES</p>
         </a>
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/phone.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">ACESSÓRIOS</p>
+        <a href="#" class="col-span-1 flex items-center flex-col  hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/phone.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">ACESSÓRIOS</p>
         </a>
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/play.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">JOGAR</p>
+        <a href="#" class="col-span-1 flex items-center flex-col hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/play.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">JOGAR</p>
         </a>
-        <a class="grid justify-items-center w-[3rem] hover:cursor-pointer hover:underline decoration-4 decoration-[#054b16]">
-            <img src="/home/jopa/universe-games/src/assets/user.svg" alt="">
-            <p class="inline-block text-center text-[1.5rem] font-bold  text-[#054b16]">ENTRAR</p>
+        <a href="#" class="col-span-1 flex items-center flex-col hover:underline decoration-4 decoration-[#054b16]">
+            <img src="../../assets/user.svg" alt="" class="w-[70px] sm:w-[90px] md:w-[70px]">
+            <p class="inline-block text-center text-[1rem] font-semibold  text-[#054b16]">ENTRAR</p>
         </a>
+        <div class="col-span-3 min-[1060px]:col-span-1"></div>
     </div>
-    
-    
-    
-
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
     name: 'Icons',
@@ -42,4 +40,3 @@ export default defineComponent({
 })
 
 </script>
-
