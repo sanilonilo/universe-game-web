87b2e2fd0a58c236f0caff51cffd582d208785fa
Sanilo Alves <saniloalves0@gmail.com>
2024-07-25 21:01:31 -0300

    style: add hover effect in icon links

--- src/components/social/SocialNetwork.vue
+++ src/components/social/SocialNetwork.vue
@@ -4,25 +4,25 @@
       <div class="flex items-center space-x-2">
         <span class="text-black text-[15px] pr-[12px]">Seguir o Xbox</span>
         <div class="flex items-start justify-start text-black text-[20px] pr-[12px] space-x-4">
-          <a href="Email" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="Email" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faEnvelope"/>
           </a>
-          <a href="Facebook" class=" w-[32px] h-[32px] cursor-pointer">
+          <a href="Facebook" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faFacebookF"/>
           </a>
-          <a href="Twitter" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="Twitter" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faXTwitter"/>
           </a>
-          <a href="Instagram" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="Instagram" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faInstagram"/>
           </a>
-          <a href="Whatsapp" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="Whatsapp" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faWhatsapp"/>
           </a>
-          <a href="TikTok" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="TikTok" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faTiktok"/>
           </a>
-          <a href="YouTube" class="w-[32px] h-[32px] cursor-pointer">
+          <a href="YouTube" class="w-[32px] h-[32px] cursor-pointer hover:bg-slate-200 flex items-center justify-center">
             <font-awesome-icon :icon="faYoutube"/>
           </a>
         </div>
@@ -35,11 +35,22 @@
 import {defineComponent} from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+///@ts-ignore
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+///@ts-ignore
 import { faXTwitter} from '@fortawesome/free-brands-svg-icons/faXTwitter';
+///@ts-ignore
+
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+///@ts-ignore
+
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+///@ts-ignore
+
 import { faTiktok } from '@fortawesome/free-brands-svg-icons/faTiktok';
+
+///@ts-ignore
+
 import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube';
 
 
