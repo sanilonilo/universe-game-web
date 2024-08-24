60f5efa505e1161acf84a8c2356bc1b0968f1296
Sanilo Alves <saniloalves0@gmail.com>
2024-08-23 22:25:26 -0300

    refactoring imports in the views

---
+++ src/presentation/components/views/index.ts
@@ -0,0 +1,16 @@
+import ItemCardGridView from './cards/item-card-grid-view.vue'
+import HeaderView from "./layout/header-view.vue"
+import BannerView from './corousel/main-carousel-view.vue'
+import IconBarView from './icon-links/icon-bar-view.vue'
+import SocialNetworkView from "./social-links/social-network-view.vue"
+import FooterView from "./layout/footer-view.vue"
+
+
+export const ComponentsViews = {
+    ItemCardGridView,
+    HeaderView,
+    BannerView,
+    IconBarView,
+    SocialNetworkView,
+    FooterView
+}
--- src/presentation/screens/controllers/home-controller.ts
+++ src/presentation/screens/controllers/home-controller.ts
@@ -1,22 +1,17 @@
 import { defineComponent } from "vue";
-import HeaderView from "../../components/views/layout/header-view.vue"
-import BannerView from '../../components/views/corousel/main-carousel-view.vue'
-import IconBarView from '../../components/views/icon-links/icon-bar-view.vue'
-import ItemCardGridView from '../../components/views/cards/item-card-grid-view.vue'
-import SocialNetworkView from "../../components/views/social-links/social-network-view.vue"
-import FooterView from "../../components/views/layout/footer-view.vue"
+import {ComponentsViews} from '../../components/views'
 import {iconsBarStore} from '../../../data/store/icons-bar-store'
 import { gridDataStore } from "../../../data/store/grid-data-store";
 
 export const HomeController = defineComponent({
     name: 'Home',
     components: {
-        HeaderView,
-        BannerView,
-        IconBarView,
-        ItemCardGridView,
-        SocialNetworkView,
-        FooterView
+        HeaderView: ComponentsViews.HeaderView,
+        BannerView: ComponentsViews.BannerView,
+        IconBarView: ComponentsViews.IconBarView,
+        ItemCardGridView: ComponentsViews.ItemCardGridView,
+        SocialNetworkView: ComponentsViews.SocialNetworkView,
+        FooterView: ComponentsViews.FooterView
     },
     data(){
         return {
